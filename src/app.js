306b58be2f58b892793df269db9bd1a7d1b8e15c
Prabha Kumar <kprabhat628@gmail.com>
2025-12-12 20:13:52 +0530

    add cors setting for production

--- src/app.js
+++ src/app.js
@@ -9,11 +9,34 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
+const allowedOrigins = [
+  process.env.CLIENT_URL_LOCAL, // Local web
+  "http://localhost:3000", // Next.js (optional)
+  process.env.EXPO_URL_LOCAL, // Expo
+  "http://*", // Expo on device
+  process.env.CLIENT_URL_PROD, // Production
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      const isAllowed = allowedOrigins.some((o) => {
+        if (!o) return false;
+        return origin.startsWith(o);
+      });
+
+      if (isAllowed) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("CORS Not Allowed: " + origin), false);
+      }
+    },
     credentials: true,
-    methods: ["POST", "PUT", "UPDATE", "DELETE"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
